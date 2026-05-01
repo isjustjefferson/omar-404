f974c783f9705b7da68ba548ab40244e5280983b
Jefferson <boarhat2018@gmail.com>
2026-05-01 17:34:30 -0300

    feat(cliente): adição de validação de CPF com Brasil API e relacionamento com entidade Falecido

--- backend/models/Cliente.js
+++ backend/models/Cliente.js
@@ -1,9 +1,34 @@
 const db = require('../config/db.js');
+const axios = require('axios');
+
+function validarCPF(cpf) {
+    const cpfLimpo = cpf.replace(/[.\-]/g, '');
+
+    try {
+        const response = await axios.get(`https://brasilapi.com.br/api/cpf/v1/${cpfLimpo}`);
+        return response.status === 200;
+    } catch (err) {
+        return false;
+    }
+}
 
 const Cliente = {
     async criar({ nome, cpf, telefone, email }) {
         if (!nome || !cpf) {
             throw new Error('Nome e CPF são obrigatórios.');
+        }
+
+        const cpfValido = await validarCPF(cpf);
+        if (!cpfValido) {
+            throw new Error('CPF inválido ou não encontrado.');
+        }
+
+        const existe = await db.query(
+            `SELECT id FROM clientes WHER cpf = $1`,
+            [cpf]
+        );
+        if (existe.rows.length > 0) {
+            throw new Error('CPF já cadastrado.');
         }
 
         const result = await db.query(
@@ -21,6 +46,30 @@ const Cliente = {
         return result.rows;
     },
 
+    async buscarPorId(id) {
+        const result = await db.query(
+            `SELECT * FROM clientes WHERE id $1`,
+            [id]
+        );
+        return result.rows[0];
+    },
+
+    async buscarComFalecidos(id) {
+        const cliente = buscarPorId(id);
+
+        if (cliente.rows.length === 0) return null;
+
+        const falecidos = await db.query(
+            `SELECT * FROM falecidos WHERE cliente_id = $1 ORDER BY criado_em DESC`,
+            [id]
+        );
+
+        return {
+            ...cliente.rows[0],
+            falecidos: falecidos.rows
+        };
+    },
+
     async atualizar(id, { nome, telefone, email }) {
         const result = await db.query(
             `UPDATE cliente SET nome = $1, telefone = $2, email = $3
@@ -28,6 +77,13 @@ const Cliente = {
             [nome, telefone, email, id]
         );
         return result.rows[0];
+    },
+
+    async deletar(id) {
+        await db.query(
+            `DELETE FROM clientes WHERE id = $1`,
+            [id]
+        );
     }
 };
 
