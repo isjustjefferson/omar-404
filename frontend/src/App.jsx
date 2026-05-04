8f436f9637be4a1dbb540db54ea1cf7e6d7262b7
Elton Santos <eess4@discente.ifpe.edu.br>
2026-05-04 02:06:05 -0300

    adição instância Axios, interceptors e proteção de rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
+import PrivateRoute from './components/PrivateRoute'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Perfil from './pages/Perfil'
@@ -12,14 +13,18 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="perfil" element={<Perfil />} />
-        <Route path="falecidos" element={<Falecidos />} />
-        <Route path="clientes" element={<Clientes />} />
-        <Route path="contratos" element={<Contratos />} />
-        <Route path="404" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
+
+      {/* Rotas protegidas — redireciona para /login se não tiver token */}
+      <Route element={<PrivateRoute />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="perfil" element={<Perfil />} />
+          <Route path="falecidos" element={<Falecidos />} />
+          <Route path="clientes" element={<Clientes />} />
+          <Route path="contratos" element={<Contratos />} />
+          <Route path="404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
+        </Route>
       </Route>
     </Routes>
   )
